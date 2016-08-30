bb4b8edcf0eea621eb304d9d8005cd6fe8f80275
Vicer <0oVicero0@users.noreply.github.com>
2016-08-31 00:53:12 +0800

    Add files via upload

---
+++ Aria2c.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Aria2cEXE=`which aria2c`
+if [[ "$Aria2cEXE" == "" ]]; then
+apt-get update
+apt-get install -y aria2
+fi
+
+rm -rf /root/.aria >/dev/null 2>&1
+rm -rf /etc/aria2 >/dev/null 2>&1
+mkdir -p /root/.aria
+mkdir -p /etc/aria2
+
+wget --no-check-certificate -q -O /root/.aria/dht.dat "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/.aria/dht.dat"
+wget --no-check-certificate -q -O /etc/aria2/aria2c "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c"
+wget --no-check-certificate -q -O /etc/aria2/aria2c.conf "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c.conf"
+
+chmod -R 755 /etc/aria2
+chmod -R +X /etc/aria2
+ln -sf /etc/aria2/aria2c /etc/init.d/aria2c
+update-rc.d aria2c defaults
+/etc/init.d/aria2c start
