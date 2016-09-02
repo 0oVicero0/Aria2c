5e617bf5b391fa8c47db24a32daa6a49120d36da
Vicer <0oVicero0@users.noreply.github.com>
2016-09-02 11:47:15 +0800

    Add files via upload

---
+++ BaiduYun.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+function Hello()
+{
+CheckEXE=`ps -e |grep "bypy.py"`
+if [[ $CheckEXE == "" ]]; then
+CreatList;
+else
+exit 1
+fi
+}
+
+function CreatList()
+{
+rm -rf /usr/share/nginx/www/list.txt >/dev/null 2>&1
+rm -rf /usr/share/nginx/www/nolist.txt >/dev/null 2>&1
+dir /usr/share/nginx/www/download > /usr/share/nginx/www/list.txt
+dir /usr/share/nginx/www/download |awk "/\.aria2/" |awk -F '.aria2' '{ print $1 }' > /usr/share/nginx/www/nolist.txt
+UpLoadFile;
+}
+
+function UpLoadFile()
+{
+NoList=`sed -n 1p /usr/share/nginx/www/nolist.txt`
+if [[ $NoList != "" ]]; then
+sed -i "/$NoList/"d /usr/share/nginx/www/list.txt
+fi
+sed -i '/^$/'d /usr/share/nginx/www/list.txt
+UpFile=`sed -n 1p /usr/share/nginx/www/list.txt`
+if [[ $UpFile != "" ]]; then
+bypy.py -v upload "/usr/share/nginx/www/download/$UpFile"
+sed -i "/$UpFile/"d /usr/share/nginx/www/list.txt
+UpLoadFile;
+else
+rm -rf /usr/share/nginx/www/list.txt >/dev/null 2>&1
+rm -rf /usr/share/nginx/www/nolist.txt >/dev/null 2>&1
+exit 1
+fi
+}
+
+Hello;
