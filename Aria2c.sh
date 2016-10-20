845cfe199c851bf2c599af037fe69862a0872d63
Vicer <0oVicero0@users.noreply.github.com>
2016-10-20 20:03:18 +0800

    Add files via upload

--- Aria2c.sh
+++ Aria2c.sh
@@ -1,25 +1,54 @@
 #!/bin/bash
 
-Aria2cEXE=`which aria2c`
-apt-get update
-apt-get install -y libssl-dev libexpat1-dev libssh2-1-dev libc-ares-dev zlib1g-dev libsqlite3-dev pkg-config
-if [[ "$Aria2cEXE" == "" ]]; then
+function Install-by-yourself()
+{
+rm -rf /usr/local/share/man/man1/aria2c.1 >/dev/null 2>&1
+rm -rf /usr/local/share/man/pt/man1/aria2c.1 >/dev/null 2>&1
+rm -rf /usr/local/share/man/ru/man1/aria2c.1 >/dev/null 2>&1
+rm -rf /usr/local/bin/aria2c >/dev/null 2>&1
+apt-get install -y libgnutls-dev libgcrypt-dev libssl-dev libexpat1-dev libssh2-1-dev libc-ares-dev zlib1g-dev libsqlite3-dev pkg-config
 cd /root
 rm -rf aria2-*
-wget wget --no-check-certificate -O aria2-release.tar.gz "https://github.com/aria2/aria2/archive/release-1.18.10.tar.gz"
+wget --no-check-certificate -O aria2-release.tar.gz "http://http.debian.net/debian/pool/main/a/aria2/aria2_1.18.8.orig.tar.bz2"
 tar -xvf aria2-*.tar.gz
 cd aria2*
 ./configure --prefix=/usr/local --with-ca-bundle='/etc/ssl/certs/ca-certificates.crt'
 make && make install
 rm -rf /root/aria2*
+}
+
+function Install-by-itself()
+{
+apt-get install -y aria2
+}
+
+Aria2cEXE=`which aria2c`
+UnZipEXE=`which unzip`
+
+if [[ "$Aria2cEXE" == "" ]]; then
+apt-get update
+if [[ "$1" == 'manual' ]]; then
+Install-by-yourself
+else
+Install-by-itself
+fi fi
+
+if [[ "$UnZipEXE" == "" ]]; then
+apt-get install -y unzip
 fi
+
 rm -rf /root/.aria2 >/dev/null 2>&1
 rm -rf /etc/aria2 >/dev/null 2>&1
 mkdir -p /root/.aria2
 mkdir -p /etc/aria2
+mkdir -p /etc/aria2/web
+wget --no-check-certificate -q -O /root/Aria2c.zip "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/Aria2c.zip"
 wget --no-check-certificate -q -O /root/.aria2/dht.dat "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/.aria2/dht.dat"
 wget --no-check-certificate -q -O /etc/aria2/aria2c "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c"
 wget --no-check-certificate -q -O /etc/aria2/aria2c.conf "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c.conf"
+
+unzip -d /etc/aria2/web /root/Aria2c.zip
+rm -rf /root/Aria2c.zip >/dev/null 2>&1
 
 chmod -R 755 /etc/aria2
 chmod -R +X /etc/aria2
