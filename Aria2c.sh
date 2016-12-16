fbe75d17d429d3545f142f7a078cd71a01a6a24a
Vicer <0oVicero0@users.noreply.github.com>
2016-12-17 01:21:55 +0800

    Update Aria2c.sh

--- Aria2c.sh
+++ Aria2c.sh
@@ -3,33 +3,20 @@
 function Install-Check()
 {
 Aria2cEXE=`which aria2c`
-UnZipEXE=`which unzip`
 }
 
 function Clean()
 {
 CleanAuto;
-rm -rf /usr/local/share/man/man1/aria2c.1 >/dev/null 2>&1
-rm -rf /usr/local/share/man/pt/man1/aria2c.1 >/dev/null 2>&1
-rm -rf /usr/local/share/man/ru/man1/aria2c.1 >/dev/null 2>&1
+rm -rf /usr/local/share/man/man1/aria2c* >/dev/null 2>&1
+rm -rf /usr/local/share/man/pt/man1/aria2c* >/dev/null 2>&1
+rm -rf /usr/local/share/man/ru/man1/aria2c* >/dev/null 2>&1
 rm -rf /usr/local/bin/aria2c >/dev/null 2>&1
 rm -rf /etc/aria2 >/dev/null 2>&1
 apt-get purge -y aria2 >/dev/null 2>&1
 apt-get autoremove -y aria2 >/dev/null 2>&1
 rm -rf /root/aria2* >/dev/null 2>&1
 Aria2cEXE=""
-}
-
-function CleanAuto()
-{
-rm -rf /etc/init.d/aria2* >/dev/null 2>&1
-rm -rf /etc/rc0.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc1.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc2.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc3.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc4.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc5.d/*aria2* >/dev/null 2>&1
-rm -rf /etc/rc6.d/*aria2* >/dev/null 2>&1
 }
 
 function Install-by-itself()
@@ -48,7 +35,6 @@ rm -rf /root/aria2-*
 wget --no-check-certificate -q -O aria2-release.tar.gz "http://http.debian.net/debian/pool/main/a/aria2/$Ver"
 tar -xvf aria2-release.tar.gz
 cd aria2*
-sed -i s'/1\, 16\,/1\, 64\,/' ./src/OptionHandlerFactory.cc
 autoreconf -i
 ./configure --prefix=/usr/local --with-libxml2 --with-ca-bundle='/etc/ssl/certs/ca-certificates.crt'
 make && make install
@@ -62,36 +48,9 @@ chmod -R 755 /etc/aria2/
 chmod +x /etc/aria2/aria2c
 chmod 777 /etc/aria2/aria2c
 ln -sf /etc/aria2/aria2c /usr/local/bin/aria2
-AutoDepend="/etc/init.d/.depend.start"
-ln -sf /etc/aria2/aria2c /etc/init.d/aria2
-ln -sf /etc/init.d/aria2 /etc/rc0.d/K01aria2
-ln -sf /etc/init.d/aria2 /etc/rc1.d/K01aria2
-ln -sf /etc/init.d/aria2 /etc/rc2.d/S15aria2
-ln -sf /etc/init.d/aria2 /etc/rc3.d/S15aria2
-ln -sf /etc/init.d/aria2 /etc/rc4.d/S15aria2
-ln -sf /etc/init.d/aria2 /etc/rc5.d/S15aria2
-ln -sf /etc/init.d/aria2 /etc/rc6.d/K01aria2
-if [ -f $AutoDepend ]; then
-sed -i '1s/aria2//g;1s/sudo/& aria2/g;/^aria2/d' $AutoDepend
-else
-sudo update-rc.d aria2c defaults
+update-rc.d -f aria2 remove
+update-rc.d aria2 defaults
 fi
-}
-
-function Install-WEB()
-{
-[ -z "$UnZipEXE" ] && apt-get install -y unzip
-[ ! -f "/root/.aria2/dht.dat" ] || rm -rf /root/.aria2 >/dev/null 2>&1
-rm -rf /etc/aria2 >/dev/null 2>&1
-mkdir -p /root/.aria2
-mkdir -p /etc/aria2
-mkdir -p /etc/aria2/web
-wget --no-check-certificate -q -O /root/Aria2c.zip "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/Aria2c.zip"
-wget --no-check-certificate -q -O /etc/aria2/aria2c "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c"
-wget --no-check-certificate -q -O /etc/aria2/aria2c.conf "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/aria2c.conf"
-[ ! -f "/root/.aria2/dht.dat" ] || wget --no-check-certificate -q -O /root/.aria2/dht.dat "https://raw.githubusercontent.com/0oVicero0/Aria2c/master/.aria2/dht.dat"
-unzip -d /etc/aria2/web /root/Aria2c.zip >/dev/null 2>&1
-rm -rf /root/Aria2c.zip >/dev/null 2>&1
 }
 
 Install-Check;
@@ -109,7 +68,6 @@ Install-by-yourself;
 else
 Install-by-itself;
 fi
-Install-WEB;
 Install-Auto;
  
 
